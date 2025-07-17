47edffb5ee93c33c3537116b4d9f0da36768fbb0
nalegke30 <auau84700@gmail.com>
2025-07-17 11:58:32 +0400

    Update project.py

--- main.py
+++ main.py
@@ -1,60 +1,106 @@
-preparation = False
-try:
-    coordinats = input('Привет, введи координаты фигуры, например(b3) и программа покажет все возможные перемещения на шахматной доске: ')
-    name = input('Также введи название фигуры: ').lower()
-    if coordinats[0] in ('abcdefgh') and int(coordinats[1]) in (12345678):
-        preparation = True
-except TypeError:
-    print('Произошла ошибка, повторите еще раз')
-except ValueError:
-    print('Произошла ошибка, пожалуйста введите координаты фиугры')
-x = '87654321'.index(coordinats[1])
-y = 'abcdefgh'.index(coordinats[0])  
+import random
+import time
+import sys
+import csv
+
+
+
+print('Hello, in this app you can learn your unknown words')
+name, surname = input('Enter your name and surname: ').split()
+mainbody = input('if you want to know the main functional of this app - enter "bam": to exit - enter "gg": else to learn "l": ')
+
+
+if mainbody == 'bam':
+    print('it"s the main body')
+    print(f"Your name is {name.title()}, surname is {surname.title()}")
+    print('this app was creatured by andrew')
+    print('Owner tg"s - "https://t.me/nalegke30"')
+
+elif mainbody == 'gg':
+    print('Вы покинули приложение')
+    sys.exit()
+
+elif mainbody == 'l':
+    print('Enter words like(apple, яблоко), when you finish you have to press "Enter" -> "Ctrl Z" -> "Enter": ')
+    data = [i.strip().split(', ') for i in sys.stdin]
+    columns = ['word','translation']
+    with open('information.csv','w',encoding='utf-8',newline='') as file:
+        file = csv.writer(file)
+        file.writerow(columns)
+        for i in data:
+            file.writerow(i)
+    elseOrNot = input('will you be learning with attempts? yes = y, no = n: ').lower()
+    if elseOrNot == 'y':
+        with open('information.csv','r',encoding='utf-8') as info:
+            info = csv.DictReader(info)
+            for i in info:
+                print(f"{i}") 
+            Continue = input('Are all words correct? and let"s start the circle of learning yes = y,no = n: ').lower()
+            if Continue == 'y' or Continue == 'yes':
+                with open('information.csv','r',encoding='utf-8') as info:
+                    info = csv.DictReader(info)
+                    lstMeanings = []
+                    lstWords = []
+                    for i in info:
+                        lstMeanings.append(i['translation'])
+                        lstWords.append(i['word'])
+                attempt = input('how many attempts do you want to apply? ')
+                count = 0
+                for word, meaning in zip(lstWords,lstMeanings):
+                    attempts = int(attempt)
+                    while attempts > 0:
+                        mean = input(f'Enter translate of this word {word}').strip()
+                        if mean == meaning:
+                            print('Correct')
+                            count += 1
+                            break
+                        else:
+                            attempts -= 1
+                            print(f'you answered uncorrectly, you have {attempts}')
+                print(f'Oh, you know {count} words')
+    elif elseOrNot == 'n':
+        with open('information.csv','r',encoding='utf-8') as info:
+                    info = csv.DictReader(info)
+                    lstMeanings = []
+                    lstWords = []
+                    for i in info:
+                        lstMeanings.append(i['translation'])
+                        lstWords.append(i['word'])
+        indexes = list(range(len(lstWords)))
+        random.shuffle(indexes)
+        lstWords = [lstWords[i] for i in indexes]
+        lstMeanings = [lstMeanings[j] for j in indexes]
+        
+        ln_lstWords = len(lstWords)
+        cor = 0
+        timer = 0
+        while ln_lstWords > 0:
+            print(f"Write meanings or translations of these words -> {lstWords}") 
+            start = time.monotonic()
+            answers = input('write your answers by comma: ').strip()
+            changed_answers = [row.strip() for row in answers.split(',')]
+            if changed_answers == lstMeanings:
+                cor += 1
+                end = time.monotonic()
+                plenty_of_time = end - start
+                timer += plenty_of_time
+                print(f"Corrrect! You solved it for {round(plenty_of_time,2)} seconds")
+                ln_lstWords -= 1 
+                break
+            else:
+                print('Try again')
+        
+        if round(plenty_of_time) > 60:
+            minutes = plenty_of_time // 60
+            seconds = plenty_of_time % 60
+            print(f'From {len(lstWords)} words you have correctly answered for {minutes} minutes and {seconds} seconds')
+        else:
+            print(f'From {len(lstWords)} words you have correctly answered for {round(plenty_of_time,2)} seconds')
     
-matrix = [['.'] * 8 for i in range(8)]
+    else:
+        print('PLEASE ENTER Y OR N (sorry for high voice)')
 
-if name == 'ладья':
-    for i in range(8):
-        for j in range(8):
-            if x == i or y ==j:
-                matrix[i][j] = '*'
-    matrix[x][y] = 'R'
-    for i in matrix:
-        print(*i)
+else:
+    print('Bro, enter correct commands!')
 
-elif name == 'ферзь':
-    for i in range(8):
-        for j in range(8):
-            if abs(x-i) == abs(y-j) or x == i or y == j:
-                matrix[i][j] = '*'
-    matrix[x][y] = 'Q'
-    for row in matrix:
-        print(*row)
-
-elif name == 'конь':
-    for i in range(8):
-        for j in range(8):
-            if (x-j)*(y-i) == 2 or (x-j)*(y-i) == -2:
-                matrix[j][i] = '*'
-    matrix[x][y] = 'N'
-    for row in matrix:
-        print(' '.join(row))
-
-elif name == 'слон':
-    for i in range(8):
-        for j in range(8):
-            if abs(x-i) == abs(y-j):
-                matrix[i][j] = '*'
-    matrix[x][y] = 'E'
-    for row in matrix:
-        print(*row)
-
-elif name == 'король':
-    for i in range(8):
-        for j in range(8):
-            if x - 1 <= i <= x + 1 and y - 1 <= j <= y + 1:
-                matrix[i][j] = '*'
-    matrix[x][y] = 'K'
-    for row in matrix:
-        print(*row)
     
